e2674a45d0036139be2bb06652dcf1130b2a51ae
Nabeel Sulieman <nabsul@outlook.com>
2016-09-19 11:15:25 -0700

    fix path name of zip file

--- migrations/wp-web/0001.sh
+++ migrations/wp-web/0001.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get install zip
-unzip /root/migrations/wordpress/latest.zip
+unzip /root/migrations/wp-web/latest.zip
 mv wordpress /var/www
 chown nobody:nogroup /var/www
 systemctl restart nginx
